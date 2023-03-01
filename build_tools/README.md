207d203e4f54b7ebce29c6c4abffe45d08d64ff7
ruizmic0100 <ruizmic0100@hotmail.com>
2023-03-01 11:15:17 -0600

    Practed linux shell

---
+++ build_tools/README.md
@@ -0,0 +1,9 @@
+# Linux Command
+
+## Test - shell builtin
+
+| Expression      | Description |
+| ----------- | ----------- |
+| -d file     | True if file is a directory.       |
+| -e file     | True if file exists.        |
+| file1 -nt file2 | True if file1 is newer than file2 |
--- build_tools/sysinfo_page.sh
+++ build_tools/sysinfo_page.sh
@@ -2,9 +2,46 @@
 
 # sysinfo_page - A script to produce a html file
 
+##### Constants
+
 title="My System Information"
 RIGHT_NOW="$(date +"%x %r %Z")"
 TIME_STAMP="Update on $right_now by $USER"
+
+##### Functions
+
+system_info()
+{
+	echo "<h2>System release info</h2>"
+	echo "<p>Funciton not yet implemented</p>"
+}
+
+show_uptime()
+{
+	echo "<h2>System uptime</h2>"
+	echo "<pre>"
+	uptime
+	echo "</pre>"
+}
+
+drive_space()
+{
+	echo "<h2>Filesystem space</h2>"
+	echo "<pre>"
+	df
+	echo "</pre>"
+}
+
+home_space()
+{
+	echo "<h2>Home directory space by user</h2>"
+	echo "<pre>"
+	echo "Bytes Directory"
+	du -s /home/* | sort -nr
+	echo "</pre>"
+}
+
+##### Main
 
 cat <<- _EOF_
 	<html>
@@ -17,6 +54,10 @@ cat <<- _EOF_
 	<body>
 	<h1>$title $HOSTNAME</h1>
 	<p>$TIME_STAMP</p>
+	$(system_info)
+	$(show_uptime)
+	$(drive_space)
+	$(home_space)
 	</body>
 	</html>
 _EOF_
